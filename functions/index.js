d285109aafcbb917b9f902f219ac1b9ca24e6835
Crystal Danger <cdneal091@gmail.com>
2020-03-30 17:04:08 -0400

    add firebase-admin for firestore

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express()
+
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
 
 app.get('/', (req, res) => {
   res.send('im the base route')
