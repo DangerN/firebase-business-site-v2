97ff51af7fe3c56104beb5f4d1f7fcdd50e5d632
Crystal Danger <cdneal091@gmail.com>
2020-03-30 16:42:10 -0400

    added test

---
+++ functions/test/index.test.js
@@ -0,0 +1,7 @@
+const test = require('firebase-functions-test')({
+  databaseURL: 'https://my-project.firebaseio.com',
+  storageBucket: 'my-project.appspot.com',
+  projectId: 'my-project',
+}, 'path/to/serviceAccountKey.json');
+
+const myFunctions = require('../index.js');
